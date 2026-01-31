e44905502c2d20901d65c8223abae9053d9dd1e3
Ricardo De Souza Junior <ricardosouzaj26@gmail.com>
2026-01-31 14:31:37 -0300

    Integrate sound alert in timer function
    Added sound notification when the timer ends.

--- timer.py
+++ timer.py
@@ -1,14 +1,16 @@
 #Timer
 import time
+from playsound import playsound
 
 def timer(segundos):
-    while segundos >= 0:
+    while segundos > 0:
         minutos, segundos_restantes = divmod(segundos, 60)
         timer = "{:02d}:{:02d}".format(minutos, segundos_restantes)
         print(timer, end="\r")
         time.sleep(1)
         segundos -= 1
     print("Tempo esgotado!!")
+    playsound("C:\\Users\\Vaio\\OneDrive\\Documentos\\alarme.mp3") #caminho para seu arquivo .mp3
         
 def main():
     t = input("insira o tempo em segundos(s):")
@@ -18,7 +20,9 @@ def main():
         return
 
     timer(int(t))
+    
 
 if __name__ == "__main__":
     main()
     
+
